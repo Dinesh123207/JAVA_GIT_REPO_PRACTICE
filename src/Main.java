f1f44edab274ff6841476fc4d452f728a73397bf
Dinesh Kumar <66234432+Dinesh123207@users.noreply.github.com>
2023-07-10 15:00:14 +0530

    FIRST COMMIT

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+//add two numbers
+import java.util.*;
+public  class Main {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println(sum);
     }
 }
